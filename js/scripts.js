71fef76e028f0d2f75e57071289b6ad735fd7305
Hatopoppo-san <73891874+Hatopoppo-san@users.noreply.github.com>
2020-11-23 18:13:54 +0100

    Added 5pokemonList
    In order to achieve task 2 on Javascript in CF

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
-alert('Hello, world!');
-let favouriteFood = 'steak';
-document.write(favouriteFood);
+let pokemonList =[]
+pokemonList = {
+  name: "bulbasaur", height: 7, type: ["glass", "poison"]
+};
+
+pokemonList = {
+  name: "charizard", height: 6, type: "fire"
+};
+
+pokemonList = {
+  name: "squirtle", height: 5, type: "water"
+};
+
+pokemonList = {
+  name: "caterpie", height: 3, type: "bug"
+};
+
+pokemonlist = {
+  name: "weedle", height: 3, type: ["bug", "poison"]
+};
